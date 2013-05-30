ac7161cf26e2687c61b40359927321f9c71844b9
Stas <s.tkachev@gmail.com>
2013-05-30 19:04:39 +0200

    missing standard tag lib added

---
+++ grails-app/taglib/gasty/std/StdTagLib.groovy
@@ -0,0 +1,34 @@
+package gasty.std
+
+import gasty.RenderingHelper
+import com.jasty.components.std.Button
+import com.jasty.components.std.Link
+import com.jasty.components.std.TextBox
+import com.jasty.components.std.ComboBox
+import com.jasty.components.std.CheckBox
+
+class StdTagLib {
+
+    static namespace = "jasty_std"
+
+    def button = { attrs ->
+        RenderingHelper.render(Button, out, attrs, false)
+    }
+
+    def link = { attrs, body ->
+        RenderingHelper.render(Link, out, attrs, body, false)
+    }
+
+    def textBox = { attrs ->
+        RenderingHelper.render(TextBox, out, attrs, !attrs.rows)
+    }
+
+    def comboBox = { attrs ->
+        RenderingHelper.render(ComboBox, out, attrs, null, false)
+    }
+
+    def checkBox = { attrs ->
+        RenderingHelper.render(CheckBox, out, attrs, true)
+    }
+
+}
